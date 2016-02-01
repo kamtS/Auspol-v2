0e87775d886565a4cc5303a1b18348f0414b76df
kamts <dan91johnson@gmail.com>
2016-02-01 01:23:18 +0000

    improved js - search bar

--- js/index.js
+++ js/index.js
@@ -25,12 +25,34 @@ $('document').ready(function() {
 
 	]
 
+/* OK so, when a user enters a postcode I only want them to see Politicians with a matching postcode
+as identified in the array "pollieProfiles" So if I create a "master array" that then filters only objects with matching postcodes
+into a 2nd array, the spindr app then pulls its data from the 2nd "dynamic" array.
+
+So to do that...
+
+IF userInput = pollieProfiles.postcode THEN copy all that = pollProfiles.postcode to dynamicPollieArray
+
+
+
+var dynamicPollieArray [
+	{
+		
+	},
+] 
+
+
+*/
 
 //Spindr postcode search bar
 	$('#spindrApp').hide();
 
 	$('#submitButton').click(function(e) {
 		e.preventDefault();
+
+		var userInput = $('#postcodeSearch').val();
+		userInput = userInput.trim();
+
 		$('#spindrApp').show();
 	});
 
